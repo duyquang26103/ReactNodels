51a7c553eac25a814f293ca234592c598d10cc86
duyquang26103 <duyquang26103@gmail.com>
2022-07-05 00:24:21 +0700

    Create, Update and Read data with Sequelize

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 const db = require("../models/index");
+import CRUDService from '../services/CRUDService';
 let getHomePage = async (req,res) => {
     try{
         let data = await db.User.findAll();
@@ -12,6 +13,50 @@ let getHomePage = async (req,res) => {
     }
 }
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud from server');
+}
+
+
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser();
+    return res.render('displayCRUD.ejs',{
+        dataTable: data
+    });
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if(userId){
+        let userData = await CRUDService.getUserInfoById(userId);
+        return res.render('editCRUD.ejs', {
+            user: userData
+        })
+
+    }else {
+        return res.send('User not found!');
+    }
+}
+let putCRUD = async (req, res) => {
+    // let data = await CRUDService.getAllUser();
+    let putData = await req.body;
+    let newUser = await CRUDService.updateUserData(putData);
+    return res.render('displayCRUD.ejs',{
+        dataTable: newUser
+    })
+}
+
 module.exports = {
     getHomePage: getHomePage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD
 }
